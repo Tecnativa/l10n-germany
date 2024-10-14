614639d0889606df8b00ed6a8913a54c1ac4bef9
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-10-14 19:40:30 +0200

    [FIX] datev_export_xml: Sort properly
    sorted method doesn't allow `DESC` clauses.

--- datev_export_xml/models/account_move.py
+++ datev_export_xml/models/account_move.py
@@ -66,7 +66,7 @@ class AccountMove(models.Model):
             )
 
         if pickings:
-            return pickings.sorted("date DESC")[0].date.date()
+            return pickings.sorted("date", reverse=True)[0].date.date()
 
         _logger.info("Invoice date used as delivery date.")
         return self.invoice_date
